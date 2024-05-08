3ee735b02652d164b281a4633dcaee37fe1a1a13
serenalilian <serenalilian99@gmail.com>
2024-05-08 16:18:48 +0000

    mru cache

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Least Recently Used caching module.
+"""Most Recently Used caching module.
 """
 from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """Represents an object that allows storing and
-    retrieving items from a dictionary with a LRU
+    retrieving items from a dictionary with an MRU
     removal mechanism when the limit is reached.
     """
     def __init__(self):
@@ -24,8 +24,8 @@ class LRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
